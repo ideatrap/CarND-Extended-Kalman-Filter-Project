0aebb9c84f2e2634b84c0832cf854594139084f0
joezq <zhouqi.coding@gmail.com>
2017-05-29 09:17:23 +0800

    WIP FusionEKF

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -32,11 +32,54 @@ FusionEKF::FusionEKF() {
         0, 0, 0.09;
 
   /**
-  TODO:
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
+
+    initialize variables and matrices (x, F, H_laser, Hj_, P,etc)
+initialize the Kalman filter position vector with the first sensor measurements
+modify the F and Q matrices prior to the prediction step based on the elapsed time between measurements
+call the update step for either the lidar or radar sensor measurement. Because the update step for lidar and radar are slightly different, there are different functions for updating lidar and radar.
   */
 
+
+  //reference: https://goo.gl/e64nwq
+
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+
+  Hj_ << 1, 1, 0, 0,
+         1, 1, 0, 0,
+         1, 1, 1, 1;
+
+  //create a 4D state vector
+  ekf_.x_ = VectorXd(4);
+
+  //the initial transition matrix F_
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+            0, 1, 0, 1,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
+
+  //state covariance matrix P
+  ekf_.P_ = MatrixXd(4, 4);
+  ekf_.P_ << 1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1000, 0,
+            0, 0, 0, 1000;
+
+  //measurement covariance
+  kf_.R_ = MatrixXd(2, 2);
+  kf_.R_ << 0.0225, 0,
+            0, 0.0225;
+
+  //measurement matrix
+  kf_.H_ = MatrixXd(2, 4);
+  kf_.H_ << 1, 0, 0, 0,
+            0, 1, 0, 0;
+
+  noise_ax = 9;
+  noise_ay = 9;
 
 }
 
@@ -65,15 +108,27 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
+      initialize radar reading
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+      float ro     = measurement_pack.raw_measurements_(0);
+      float phi    = measurement_pack.raw_measurements_(1);
+      float ro_dot = measurement_pack.raw_measurements_(2);
+      ekf_.x_(0) = ro     * cos(phi);
+      ekf_.x_(1) = ro     * sin(phi);
+      ekf_.x_(2) = ro_dot * cos(phi);
+      ekf_.x_(3) = ro_dot * sin(phi);
+
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      ekf_.x_(0) = measurement_pack.raw_measurements_(0);
+      ekf_.x_(1) = measurement_pack.raw_measurements_(1);
     }
 
+    previous_timestamp_ = measurement_pack.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
